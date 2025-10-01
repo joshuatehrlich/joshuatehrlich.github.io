c71aaaba701aff5aa2e68c3a0769a47df1f8f1d9
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2025-10-01 17:50:57 -0400

    fof v1

---
+++ 06_fof/script.js
@@ -0,0 +1,250 @@
+const TEXT_HOLDER = document.querySelector("#text-holder");
+
+const TEXT_SOURCE = `
+I have a theory about how the world works
+`;
+
+const sources = [
+	"source.md",
+	"source2.md"
+]
+const titles = [
+	"unknown",
+	"rain"
+]
+let currentSource = 0;
+
+let maxHeight = 0;
+
+async function loadAndParseLines(source) {
+    const response = await fetch(source);
+    const rawText = await response.text();
+    const normalizedText = rawText.replace(/\r\n|\r/g, "\n");
+    const lines = normalizedText.split(/(?<=[.!?])|\n/)
+        .map(line => line.trim())
+        .filter(line => line.length > 0);
+    // const lines = normalizedText.split("\n");
+    // console.log(lines);
+    return lines;
+}
+
+// function writeText(textSource, textHolder) {
+// 	for (let ch of textSource) {
+
+// 		const letter = document.createElement("div");
+// 		letter.className = "textLetter"
+
+// 		// textHolder.appendChild(letter);
+// 		letter.textContent = ch;
+// 		textHolder.appendChild(letter);
+		
+// 	}
+// }
+
+function createDivs(textSource, textHolder) {
+	let maxLineLength = 0;
+
+	for (let line of textSource) {
+		if (line.length > maxLineLength) {
+			maxLineLength = line.length;
+		}
+	}
+
+	maxLineLength += 2;
+
+	for (let i = 0; i < maxLineLength; i++) {
+		const line = document.createElement("div");
+		line.className = "textLine";
+		textHolder.appendChild(line);
+	}
+	return maxLineLength;
+}
+
+function writeLines(textSource, textHolder, maxLineLength) {
+	
+	let lineNumber = 0;
+
+	for (let sourceLine of textSource) {
+
+		let line = sourceLine;
+
+		let startIndex = Math.max(0, Math.floor((maxLineLength - line.length)/2)); // round down
+
+
+		for (let i = startIndex; i < (line.length+startIndex); i++) {
+
+			const char = line[i-startIndex];
+			const letter = document.createElement("div");
+			letter.className = "textLetter";
+			letter.classList.add(`line${lineNumber}`);
+
+			if (lineNumber % 2 === 0) {
+				letter.classList.add("bolded");
+			} else if ((lineNumber+1) % 4 === 0) {
+				letter.classList.add("bolded2");
+			}
+
+			letter.textContent = char + " ";
+			textHolder.children[i].appendChild(letter);
+
+		}
+
+		lineNumber++;
+	}
+
+	// let maxHeight = 0;
+	// for (let line of textHolder.children) {
+	// 	if (line.children.length > maxHeight) {
+	// 		maxHeight = line.children.length;
+	// 	}
+	// }
+
+	// for (let line of textHolder.children) {
+
+	// 	let currentLineHeight = textHolder.children.length;
+	// 	let blankHeightToTop = (currentLineHeight - maxHeight)/2;
+
+	// 	let lineCounter = blankHeightToTop;
+
+	// 	for (let letter of line.children) {
+	// 		letter.classList.add(`line${lineCounter}`);
+	// 		lineCounter++;
+	// 	}
+	// }
+
+}
+
+function adjustLines() {
+	for (let line of TEXT_HOLDER.children) {
+		if (line.children.length % 2 === 0) {
+			const newLine = document.createElement("div");	
+			newLine.className = "textLine";
+			newLine.textContent = " ";
+			line.appendChild(newLine);
+		}
+	}
+}
+
+// writeText(TEXT_SOURCE, TEXT_HOLDER);
+
+async function main() {
+
+	document.querySelector(".title").textContent = titles[currentSource];
+
+	const TEXT_SEQUENCE = await loadAndParseLines(sources[currentSource]);
+
+	const maxLineLength = createDivs(TEXT_SEQUENCE, TEXT_HOLDER);
+	writeLines(TEXT_SEQUENCE, TEXT_HOLDER, maxLineLength);
+	adjustLines();
+
+	for (let line of TEXT_HOLDER.children) {
+		if (line.children.length > maxHeight) {
+			maxHeight = line.children.length;
+		}
+	}
+}
+
+main();
+
+document.addEventListener("mousemove", (e) => {
+
+	let hoveredLetter = document.querySelector(".textLetter:hover");
+	if (!hoveredLetter) {
+		for (let line of TEXT_HOLDER.children) {
+			line.style.transform = `translateY(0px)`;
+			for (let letter of line.children) {
+				letter.classList.remove("readingLine");
+				letter.classList.remove("readingLetter");
+			}
+		}
+		return;
+	}
+
+	// grab current vertical trasnform of hovered line
+	let hoveredLetterOffset = 0;
+	let hoveredLine = hoveredLetter.parentElement;
+	const style = window.getComputedStyle(hoveredLine);
+	const transform = style.transform;
+	if (transform !== 'none') {
+	  const matrix = transform.match(/matrix.*\((.+)\)/)[1].split(', ');
+	  const translateY = parseFloat(matrix[5]); // Y translation is at index 5
+	  hoveredLetterOffset = translateY;
+	}
+
+	let hoveredClass = Array.from(hoveredLetter.classList)[1];
+
+	for (let line of TEXT_HOLDER.children) {
+		for (let letter of line.children) {
+			if (letter.classList.contains(hoveredClass)) {
+				letter.classList.add("readingLine");
+
+				if (letter.matches(":hover")) {
+					letter.classList.add("readingLetter");
+				} else {
+					letter.classList.remove("readingLetter");
+				}
+			} else {
+				letter.classList.remove("readingLine");
+				letter.classList.remove("readingLetter");
+			}
+		}
+	}
+
+	let lineHeightRelative = Array.from(hoveredLetter.parentElement.children).indexOf(hoveredLetter);
+	let lineVerticalLength = Array.from(hoveredLetter.parentElement.children).length;
+	let lineHeight = lineHeightRelative + (maxHeight - lineVerticalLength)/2;
+	// console.log(lineHeight);
+
+	for (let line of TEXT_HOLDER.children) {
+		let importantLetter = Array.from(line.children).find(letter => letter.classList.contains(hoveredClass));
+		if (!importantLetter) continue;
+
+		let localHeighRelative = Array.from(line.children).indexOf(importantLetter);
+		let localVerticalLength = Array.from(line.children).length;
+		let localLineHeight = localHeighRelative + (maxHeight - localVerticalLength)/2;
+		let lineHeightDifference = localLineHeight - lineHeight;
+
+		line.style.transform = `translateY(${lineHeightDifference * -15 + hoveredLetterOffset}px)`;
+		// line.style.transform = `translateY(${lineHeightDifference * -15}px)`;
+		// trying not to overdo the offset becacuse then the player can move the creature off the page
+	}
+
+});
+
+function highlightLine(lineNumber) {
+	for (let line of TEXT_HOLDER.children) {
+		for (let letter of line.children) {
+			if (letter.classList.contains(`line${lineNumber}`)) {
+				letter.classList.add("selectedLine");
+				console.log("added");
+			} else if (letter.classList.contains(`line${lineNumber+2}`) || letter.classList.contains(`line${lineNumber-2}`)) {
+				letter.classList.remove("selectedLine");
+				letter.classList.add("selectedLine2");
+			} else {
+				letter.classList.remove("selectedLine");
+				letter.classList.remove("selectedLine2");
+			}
+		}
+	}
+}
+
+document.addEventListener("click", () => {
+	if (!document.querySelector(".next").matches(":hover")) {
+		return;
+	}
+
+	currentSource++;
+	if (currentSource >= sources.length) {
+		currentSource = 0;
+	}
+
+	TEXT_HOLDER.innerHTML = "";
+
+	document.querySelector(".title").textContent = titles[currentSource];
+
+	let maxHeight = 0;
+
+	main();
+
+	
+});
---
+++ 06_fof/source.md
@@ -0,0 +1,62 @@
+I have a theory about how the world works
+
+It's really hard to prove
+
+I can't believe you fooled me twice
+
+I can't believe we've left the room
+
+Inside a giant flower lies a bed of birds and the flow of trees
+
+The sky, with it's bright, benevolent sun
+
+Fights back too
+
+I'm so tired of eating inside a stone hut that wraps around the edge of the world
+
+If you click here you'll die, says who?
+
+Cyra could barely see so instead she listened
+
+Cyra could barely see, so instead she listened. She heard her undulating heartbeat. She smelled gunpowder and steel, wilting as she walked behind her brother
+
+They weaved through bristling spikes that reached up and far above to crown the forest. From below, their gia
+
+Not two minutes away by foot, in the very same forest, Small Scarrow shook.
+
+He was not afraid, but physically overwhelmed. His clothing was strewn, ri
+
+Small Scarrow held a gun. He had taken it in the same manner which any person might take an object handed to them, and later be
+
+Cyra's brother, Tarin, was not aware of any of this - not even Cyra who trailed so closely behind him. Only two hours ago, he had taken a large dose of 
+
+These three - Cyra, Tarin, and Small Scarrow - had, over the past hour, reached a strange level
+
+This strange, yet explicitly ordered march was incredibly fragile. Just one shift of vision or pattern of thought might break the cycle. With the info
+
+No. I can't
+
+I can't.
+
+It's evil!
+
+The flower
+Once fought
+Against
+An orange storm
+Warmth isn't something
+You can teach, but
+it's something
+you can learn.
+
+How does a body
+of text exist
+alone and at once
+in a void
+
+I can't tell
+you why i told
+you what i told
+you i wish
+i didn't
+know
